35b54dd5f889993d9dcb27186f10a59af7654bc9
michael <mikedami001@gmail.com>
2022-12-08 01:04:56 -0800

    hope

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -print the line required by alx for the c-program using puts funtion keyword
+ *
+ * Return: 0
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
