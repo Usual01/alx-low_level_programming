75cc9a74dad8759b5bc93f7ded9a89c2a04807a0
michael <mikedami001@gmail.com>
2022-12-09 15:42:43 -0800

    kl

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - tired
+ *
+ * Return
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '8'; b++)
+		{
+			for (c = '0'; c <= '7'; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 7 || a != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
