ccb9d3320d22dca576c1178f8ad58088d0ad19e8
michael <mikedami001@gmail.com>
2022-12-09 11:21:28 -0800

    h

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 success, non zero error.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i !== '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
