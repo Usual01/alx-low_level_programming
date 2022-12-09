84267d284bc082ded7b1518a6cc15814ea7ffd24
michael <mikedami001@gmail.com>
2022-12-09 13:10:36 -0800

    hum

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - gsarri garri
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '8'; b++)
+		{
+			for (c = '0'; c <= '7'; c++)
+			{
+				if (a > b || b > c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != '7' || b != '8')
+					{
+					putchar(' ');
+					putchar(',');
+					}
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
