2d2f2facecf7abca3a09c193cb887b2f191bc9a4
michael <mikedami001@gmail.com>
2022-12-13 15:24:11 -0800

    more

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			c == b * a;
-			if (a == 0)
+			if (b == 0)
 			{
-				_putchar(a + '0');
+				_putchar(b + '0');
 			}
 			if (c < 10 && a != 0)
 			{
