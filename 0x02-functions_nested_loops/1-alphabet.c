21f6ffbb737c24352f701218ed5600e8956fa9cd
michael <mikedami001@gmail.com>
2022-12-13 07:10:39 -0800

    delete .swp

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
