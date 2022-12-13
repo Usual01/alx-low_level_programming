9e57892207d58aefd8bad26dc5bb9338395ca905
michael <mikedami001@gmail.com>
2022-12-13 12:39:48 -0800

    fg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print output
+ *@n: the check
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
