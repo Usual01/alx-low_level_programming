b5b2179d5c436b4a07254b3f5843d6d4fd28651f
michael <mikedami001@gmail.com>
2022-12-14 00:35:02 -0800

    abcd

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a, b, c, d;
+
+	b = 1;
+
+	c = 2;
+
+	for (a = 0; a < 50 ; ++i)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+	return (0);
+}
