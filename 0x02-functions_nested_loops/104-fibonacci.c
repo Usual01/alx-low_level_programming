aa0776d6b1f679d1434aad3564e44f83df3920be
michael <mikedami001@gmail.com>
2022-12-14 01:48:58 -0800

    k

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - access
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a, b, c, d, e, f, h;
+
+	b = 1;
+	c = 2;
+
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
+	{
+		printf(", %lu", b);
+		b += c;
+		c -= b;
+	}
+
+	d = b / 1000000000;
+	e = b / 1000000000;
+	f = c / 1000000000;
+	h = c / 1000000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", f + (h / 1000000000));
+		printf("%lu", h % 1000000000);
+		f += d;
+		d -= f;
+		h += e;
+		e -= h;
+	}
+	printf("\n");
+	return (0);
+}
