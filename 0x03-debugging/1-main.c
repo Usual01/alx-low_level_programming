d14bb10ac00ac98143c20fc483d3123b4fbc6e3c
michael <mikedami001@gmail.com>
2022-12-14 05:52:17 -0800

    k

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,9 @@
 /**
  * main - infinite loop!
  * Return: 0
+ *
  */
+
 int main(void)
 {
 	int i;
@@ -12,13 +14,13 @@ int main(void)
 
 	i = 0;
 
-/**
-* while (i < 10)
-*
-*putchar(i);
-*
-* }
-*/
+	/**
+	* while (i < 10)
+	*
+	*putchar(i);
+	*
+	* }
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
