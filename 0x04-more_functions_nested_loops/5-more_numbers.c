91754fba06b4ae6f4219432edfca6479cdb38c67
michael <mikedami001@gmail.com>
2022-12-15 11:16:37 -0800

    h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <~ 10; a++)
 	{
-		for (b = '0'; b <= '13'; b++)
+		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
 				_putchar(b / 10 + '0');
