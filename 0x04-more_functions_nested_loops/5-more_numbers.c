352cbc4c94b369fde4ce57cb28b280bca0b3f873
michael <mikedami001@gmail.com>
2022-12-15 10:27:02 -0800

    agogo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
 
 void more_numbers(void)
 {
-	int a, j;
+	int a, b;
 
-	for (a = '0'; a <= '10' ; b++)
+	for (a = '0'; a <= '10' ; a++)
 	{
 		for (b = '0'; b <= '14'; b++)
 		{
 			_putchar(a);
 		}
-		_putchar('\n'0
+		_putchar('\n')
 	}
 	_putchar('\n');
 }
