1ccbd87949c0cf348d1130367c4642060c636153
michael <mikedami001@gmail.com>
2022-12-15 13:09:54 -0800

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,16 @@ void print_square(int size)
 		_putchar('\n');
 	}
 	else
+	{
 		int a, b;
 
 		for (a = 0; a < size; a++)
+		{
 			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
-		_putchar('\n');
+			_putchar('\n');
+		}
+	}		
 }
