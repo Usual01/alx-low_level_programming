2f9071401b52ef7d7f7d5f1809f8e01ec5f115b1
michael <mikedami001@gmail.com>
2022-12-19 01:38:42 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses string
+ * @s: variable
+ */
+
+void rev_string(char *s)
+{
+	int  a, b;
+	char string;
+
+	for (a = 0; s[a] != '\0'; ++a)
+	{
+	}
+	for (b = 0; b < a / 2; ++b)
+	{
+		string = s[b];
+		s[i] = s[a - 1 - b];
+		s[a - 1 - b] = string;
+	}
+}
