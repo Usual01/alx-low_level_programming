230fdf17b03a6883d3d9c469fdc83e89c00c6504
michael <mikedami001@gmail.com>
2022-12-19 04:30:13 -0800

    ji

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - copies from one pointer
+* @dest: point
+* @src: pointer
+*Return: pointer
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+	return (dest);
+}
