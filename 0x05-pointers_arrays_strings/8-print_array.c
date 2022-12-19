d51a17ecade55e473ab5c8049ee288ccef5a57e1
michael <mikedami001@gmail.com>
2022-12-19 03:30:12 -0800

    eat

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of the array
+ * @n: variable
+ * @a: string parameter
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; ++b)
+	{
+		if (b != (n - 1))
+			printf("%d, ", a[b]);
+		else
+			printf("%d", a[b]);
+	}
+	printf('\n');
+}
