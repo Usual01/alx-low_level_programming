924e33f2b01f8487c4d4ae48499a5d4cdcd15560
michael <mikedami001@gmail.com>
2023-01-02 02:09:39 -0800

    ko

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,15 @@
 
 char *rot13(char *s)
 {
-	rot13(char *s)
-	{
-		int n;
 
-		for (n = 0; s[n] != '\0'; n++)
-		{
-			if ((s[n] >= 'a' && s[n] <= 'n') || (s[n] >= 'A' && s[n] <= 'N'))
-				s[n] += 13;
-			else if ((s[n] >= 'n' && s[n] <= 'z') || (s[n] >= 'N' && s[n] <= 'Z'))
-				s[n] -= 13;
-		}
-		return (s);
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		if ((s[n] >= 'a' && s[n] <= 'n') || (s[n] >= 'A' && s[n] <= 'N'))
+			s[n] += 13;
+		else if ((s[n] >= 'n' && s[n] <= 'z') || (s[n] >= 'N' && s[n] <= 'Z'))
+			s[n] -= 13;
 	}
+	return (s);
+}
