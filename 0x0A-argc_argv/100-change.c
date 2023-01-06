5ff1bad102002393f8b2c93df93aaa1381a62add
michael <mikedami001@gmail.com>
2023-01-06 13:21:06 -0800

    nobe

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,20 +20,20 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	cents = atoi(argv[1]);
-	
+		
 	while (cents > 0)
 	{
-	if (cents >= 25)
-		cents -= 25;
-	else if (cents >= 10)
-		cents -= 10;
-	else if (cents >= 5)
-		cents -= 5;
-	else if (cents >= 2)
-		cents -= 2;
-	else if (cents >= 1)
-		cents -= 1;
-	ncoins += 1;
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
 	printf("%d\n", ncoins);
 	return (0);
