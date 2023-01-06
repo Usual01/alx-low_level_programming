656676c818d97f649fedd21223be9defab6f7617
michael <mikedami001@gmail.com>
2023-01-06 04:13:44 -0800

    ho

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,11 @@
 /**
  * main - prints program name
  * @argv: array vector
+ * @argc:argument count
  * Return: 0
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
