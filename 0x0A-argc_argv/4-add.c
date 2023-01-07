1e3e01c3e139aac16bd4dc480562389faac554bd
michael <mikedami001@gmail.com>
2023-01-07 01:18:44 -0800

    k

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,12 +15,15 @@ int main(int argc, char *argv[])
 
 	for (a = 1; a < argc; a++)
 	{
+		for (b = 0; argv[a][b]; b++)
+		{
 		if (argv[a][b] < '0' || argv[a][b] > '9')
 		{
 			printf("Error\n");
 			return (1);
 		}
 		sum += atoi(argv[a]);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
