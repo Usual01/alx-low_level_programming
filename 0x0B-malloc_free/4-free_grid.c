6dbd9d5e0c8eb678e2a110cc9b6057c05d47478a
michael <mikedami001@gmail.com>
2023-01-09 18:20:10 -0800

    kl

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function
+ * @grid: MATRIX ROW
+ * @height: column
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+	int *b;
+
+	for (a = 0; a < height; a++)
+	{
+		p = grid[i];
+		free(p);
+	}
+	free(p);
+}
+
