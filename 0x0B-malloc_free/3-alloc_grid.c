a333b77065962846d82ef3f30136769f41114aa3
michael <mikedami001@gmail.com>
2023-01-09 18:50:17 -0800

    kl

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create 2d array
+ * @width: column
+ * @height: rows
+ * Return: NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i;
+	int j;
+	int l;
+	int *p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
+		for (i = 0; i < height; i++)
+		{
+			*(matrix + i) = (int *)malloc(width * sizeof(int));
+			if (*(matrix + i) == NULL)
+			{
+				for (i = 0; i < height; i++)
+				{
+					p = matrix[i];
+					free(p);
+				}
+				free(matrix);
+				return (NULL);
+			}
+		}
+		for (l = 0; l < height; l++)
+		{
+			for
+			{
+				matrix[l][j] = 0;
+			}
+		}
+		return (matrix);
+}
