eec75cff76bd41b67108a8e6737c92792084d4e8
michael <mikedami001@gmail.com>
2023-01-12 06:27:09 -0800

    lo

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,31 +11,41 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *str;
+	unsigned int str1, str2;
+
+	str1 = str2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-	if (n > ls2)
-		n = ls2;
-	lsout = ls1 + n;
+	while (s1[str1] != '\0')
+		str1 += 1;
+	while (s2[str2] != '\0')
+		str2 += 1;
+	str2++;
+	if (n > str2)
+		n = str2;
+	str = malloc(sizeof(*str) * n + (x + 1));
 
-	if (sout == NULL)
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-	sout[i] = '\0';
+	str1 = 0;
+	while (s1[str1] != '\0')
+	{
+		str[str1] = s1[str1];
+		str1 += 1;
+	}
+	while (s2[y] != '\0' && y < n)
+	{
+		str[str1] = s2[str2];
+		str1++;
+		str2++;
+	}
+	str[str1] = '\0';
 
-	return (sout);
+	return (str);
 }
 
 
