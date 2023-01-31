d909f509ce62921c17c076aef73344999567ae02
michael <mikedami001@gmail.com>
2023-01-30 23:22:57 -0800

    ko

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,23 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Locates a given node of
- * a listint_t linked list.
- * @head: A pointer to the head of the listint_t list.
- * @index: The index of the node to locate - indices start at 0.
- * Return: If the node does not exist - NULL.
- * Otherwise - the located node.
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the head of the listint_t list.
+ * Return: If the linked list is empty - 0.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int pop_listint(listint_t **head)
 {
-	unsigned int node;
+	listint_t *tmp;
+	int ret;
 
-	for (node = 0; node < index; node++)
-	{
-		if (head == NULL)
-			return (NULL);
+	if (*head == NULL)
+		return (0);
 
-		head = head->next;
-	}
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
 
-	return (head);
+	free(tmp);
+
+	return (ret);
 }
