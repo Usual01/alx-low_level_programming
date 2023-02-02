42668f274ad491f4e3b583fd05338ed8d4367d9f
michael <mikedami001@gmail.com>
2023-02-02 08:03:46 -0800

    klo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,60 +1,32 @@
-#include "main.h
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>"
-
-
 /**
- * _pow_recursion - function that returns value of x in power format
- *@x:base number
- *@y:raised to the power
- *Return: raised nuber
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
-
-/**
- *_strlen - returns a string
- *@s: pointer
- *Return: string
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * binary_to_uint- convert binary to unsigned int
+ * binary_to_uint - converts a binary number
  * @b: binary
- * Return:binary_to_uint- convert binary to unsigned int
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len, exp = 0;
-	unsigned int res = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (len-- && len >= 0)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[len] == '1')
-			res += _pow_recursion(2, exp);
-		else if (b[len] != '0')
-			return (0);
-	exp++;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);																			}
+		if (b[len] & 1)
+		{
+			ui += base_two																			}
 	}
-	return (res);
+	return (ui);
 }
 
