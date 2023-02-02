f9d1521f0450dd36f709a2ec6ad9c2a5503d8dd7
michael <mikedami001@gmail.com>
2023-02-02 09:04:01 -0800

    h

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,35 +7,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, j = strlen(b), num;
-	unsigned int sum = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (b[i] != '\0')
-	{
-		if (b[i] != '1' && b[i] != '0')
-			return (0);
-		num = b[i] - '0';
-		sum = sum + num * _pow(2, j - i - 1);
-		i++;
-	}
-	return (sum);
-}
-/**
- * _pow - to find the power
- * @a: base
- * @b:exponenet
- * Return:power
- */
-int _pow(int a, int b)
-{
-	int power = 1, i;
 
-	for (i = 1; i <= b; ++i)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		power = power * a;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (power);
+
+	return (ui);
 }
+
 
